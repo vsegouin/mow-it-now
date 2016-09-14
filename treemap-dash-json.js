4435c1297d95531d046a73dc869ae685634f7c23
ocalik <vincent.segouin@hotmail.fr>
2016-09-15 01:38:17 +0200

    Replaced default site by clover reports

---
+++ treemap-dash-json.js
@@ -0,0 +1,128 @@
+var treeMapJson = {"id":"Clover database Thu Sep 15 2016 01:35:53 CEST0","name":"","data":{
+    "$area":1006.0,"$color":65.60636,"title":
+    " 1006 Elements, 65.6% Coverage"},"children":[{"id":
+      "com.vsegouin.mowitnow.ui.gui.controllers.file781","name":
+      "com.vsegouin.mowitnow.ui.gui.controllers.file","data":{"$area":82.0,
+        "$color":0.0,"title":
+        "com.vsegouin.mowitnow.ui.gui.controllers.file 82 Elements, 0% Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.ui.gui.utils991","name":
+      "com.vsegouin.mowitnow.ui.gui.utils","data":{"$area":15.0,"$color":
+        86.666664,"title":
+        "com.vsegouin.mowitnow.ui.gui.utils 15 Elements, 86.7% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.ui.gui.controllers.remote975","name":
+      "com.vsegouin.mowitnow.ui.gui.controllers.remote","data":{"$area":
+        10.0,"$color":20.0,"title":
+        "com.vsegouin.mowitnow.ui.gui.controllers.remote 10 Elements, 20% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.services.file.interfaces537","name":
+      "com.vsegouin.mowitnow.services.file.interfaces","data":{"$area":0.0,
+        "$color":-100.0,"title":
+        "com.vsegouin.mowitnow.services.file.interfaces 0 Elements,  -  Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.ui.gui.components.visualization600","name":
+      "com.vsegouin.mowitnow.ui.gui.components.visualization","data":{
+        "$area":66.0,"$color":96.969696,"title":
+        "com.vsegouin.mowitnow.ui.gui.components.visualization 66 Elements, 97% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.ui.gui.controllers.infos863","name":
+      "com.vsegouin.mowitnow.ui.gui.controllers.infos","data":{"$area":
+        73.0,"$color":72.60274,"title":
+        "com.vsegouin.mowitnow.ui.gui.controllers.infos 73 Elements, 72.6% Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.mower.core166","name":
+      "com.vsegouin.mowitnow.mower.core","data":{"$area":57.0,"$color":
+        100.0,"title":
+        "com.vsegouin.mowitnow.mower.core 57 Elements, 100% Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.mower.core.detection223",
+      "name":"com.vsegouin.mowitnow.mower.core.detection","data":{"$area":
+        77.0,"$color":97.402596,"title":
+        "com.vsegouin.mowitnow.mower.core.detection 77 Elements, 97.4% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.services.file.exceptions533","name":
+      "com.vsegouin.mowitnow.services.file.exceptions","data":{"$area":4.0,
+        "$color":100.0,"title":
+        "com.vsegouin.mowitnow.services.file.exceptions 4 Elements, 100% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.services.language.interfaces554","name":
+      "com.vsegouin.mowitnow.services.language.interfaces","data":{"$area":
+        0.0,"$color":-100.0,"title":
+        "com.vsegouin.mowitnow.services.language.interfaces 0 Elements,  -  Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.controllers47","name":
+      "com.vsegouin.mowitnow.controllers","data":{"$area":119.0,"$color":
+        95.79832,"title":
+        "com.vsegouin.mowitnow.controllers 119 Elements, 95.8% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.services.dependencies.util442","name":
+      "com.vsegouin.mowitnow.services.dependencies.util","data":{"$area":
+        3.0,"$color":66.66667,"title":
+        "com.vsegouin.mowitnow.services.dependencies.util 3 Elements, 66.7% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.controllers.interfaces166","name":
+      "com.vsegouin.mowitnow.controllers.interfaces","data":{"$area":0.0,
+        "$color":-100.0,"title":
+        "com.vsegouin.mowitnow.controllers.interfaces 0 Elements,  -  Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.services.file445","name":
+      "com.vsegouin.mowitnow.services.file","data":{"$area":88.0,"$color":
+        69.318184,"title":
+        "com.vsegouin.mowitnow.services.file 88 Elements, 69.3% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.ui.gui.handlers.canvas985","name":
+      "com.vsegouin.mowitnow.ui.gui.handlers.canvas","data":{"$area":6.0,
+        "$color":66.66667,"title":
+        "com.vsegouin.mowitnow.ui.gui.handlers.canvas 6 Elements, 66.7% Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.services.language537",
+      "name":"com.vsegouin.mowitnow.services.language","data":{"$area":
+        17.0,"$color":88.2353,"title":
+        "com.vsegouin.mowitnow.services.language 17 Elements, 88.2% Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.ui.gui574","name":
+      "com.vsegouin.mowitnow.ui.gui","data":{"$area":26.0,"$color":0.0,
+        "title":"com.vsegouin.mowitnow.ui.gui 26 Elements, 0% Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.ui.gui.controllers666",
+      "name":"com.vsegouin.mowitnow.ui.gui.controllers","data":{"$area":
+        98.0,"$color":57.14286,"title":
+        "com.vsegouin.mowitnow.ui.gui.controllers 98 Elements, 57.1% Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.mower.exceptions425",
+      "name":"com.vsegouin.mowitnow.mower.exceptions","data":{"$area":11.0,
+        "$color":81.818184,"title":
+        "com.vsegouin.mowitnow.mower.exceptions 11 Elements, 81.8% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.ui.gui.controllers.menu936","name":
+      "com.vsegouin.mowitnow.ui.gui.controllers.menu","data":{"$area":39.0,
+        "$color":7.692308,"title":
+        "com.vsegouin.mowitnow.ui.gui.controllers.menu 39 Elements, 7.7% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.services.dependencies436","name":
+      "com.vsegouin.mowitnow.services.dependencies","data":{"$area":6.0,
+        "$color":100.0,"title":
+        "com.vsegouin.mowitnow.services.dependencies 6 Elements, 100% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.ui.gui.controllers.interfaces936","name":
+      "com.vsegouin.mowitnow.ui.gui.controllers.interfaces","data":{"$area":
+        0.0,"$color":-100.0,"title":
+        "com.vsegouin.mowitnow.ui.gui.controllers.interfaces 0 Elements,  -  Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.mower.core.movements337",
+      "name":"com.vsegouin.mowitnow.mower.core.movements","data":{"$area":
+        50.0,"$color":100.0,"title":
+        "com.vsegouin.mowitnow.mower.core.movements 50 Elements, 100% Coverage"},
+      "children":[]},{"id":
+      "com.vsegouin.mowitnow.ui.gui.controllers.actions764","name":
+      "com.vsegouin.mowitnow.ui.gui.controllers.actions","data":{"$area":
+        17.0,"$color":76.47059,"title":
+        "com.vsegouin.mowitnow.ui.gui.controllers.actions 17 Elements, 76.5% Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow0","name":
+      "com.vsegouin.mowitnow","data":{"$area":47.0,"$color":0.0,"title":
+        "com.vsegouin.mowitnow 47 Elements, 0% Coverage"},"children":[]},{
+      "id":"com.vsegouin.mowitnow.mower.enums387","name":
+      "com.vsegouin.mowitnow.mower.enums","data":{"$area":38.0,"$color":
+        94.73685,"title":
+        "com.vsegouin.mowitnow.mower.enums 38 Elements, 94.7% Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.ui.cli554","name":
+      "com.vsegouin.mowitnow.ui.cli","data":{"$area":20.0,"$color":0.0,
+        "title":"com.vsegouin.mowitnow.ui.cli 20 Elements, 0% Coverage"},
+      "children":[]},{"id":"com.vsegouin.mowitnow.mower.core.map300","name":
+      "com.vsegouin.mowitnow.mower.core.map","data":{"$area":37.0,"$color":
+        62.162163,"title":
+        "com.vsegouin.mowitnow.mower.core.map 37 Elements, 62.2% Coverage"},
+      "children":[]}]}
+;
+processTreeMapDashJson (treeMapJson);
